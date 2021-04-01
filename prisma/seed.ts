28196d6c22abc64c0674d815a00804b7a33cbaa1
yanxiong huang <huangyanxiong2013@gmail.com>
2021-04-01 16:07:20 +0800

    db:Add two users

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+async function main() {
+  const users = [
+    {
+      id: 1,
+      username: 'john',
+      password: 'changeme',
+    },
+    {
+      id: 2,
+      username: 'maria',
+      password: 'guess',
+    },
+  ];
+  const res = await prisma.user.createMany({
+    data: users,
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
