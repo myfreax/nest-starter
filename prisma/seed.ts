166ac82fe5358c3db0db605c9f8a846fc1679e57
yanxiong huang <huangyanxiong2013@gmail.com>
2021-04-14 20:37:28 +0800

    Fix: permission id unique constraint error

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,28 +1,86 @@
-import { PrismaClient, Permission, Role } from '@prisma/client';
+import { PrismaClient, Permission, PermissionAction } from '@prisma/client';
+import { CreatePermissionDto } from 'src/permissions/dto/create-permission.dto';
 const prisma = new PrismaClient();
+
 async function main() {
-  const permissions: Permission[] = [
-    { id: 1, resource: 'permission', attributes: '*', action: 'create:any' },
-    { id: 2, resource: 'permission', attributes: '*', action: 'read:any' },
-    { id: 3, resource: 'permission', attributes: '*', action: 'update:any' },
-    { id: 4, resource: 'permission', attributes: '*', action: 'delete:any' },
-    { id: 5, resource: 'role', attributes: '*', action: 'create:any' },
-    { id: 6, resource: 'role', attributes: '*', action: 'read:any' },
-    { id: 7, resource: 'role', attributes: '*', action: 'update:any' },
-    { id: 8, resource: 'role', attributes: '*', action: 'delete:any' },
-    { id: 9, resource: 'user', attributes: '*', action: 'create:any' },
-    { id: 10, resource: 'user', attributes: '*', action: 'read:any' },
-    { id: 12, resource: 'user', attributes: '*', action: 'update:any' },
-    { id: 13, resource: 'user', attributes: '*', action: 'delete:any' },
-    { id: 14, resource: 'user', attributes: '*', action: 'update:own' },
-    { id: 15, resource: 'user', attributes: '*', action: 'delete:own' },
+  const permissions: CreatePermissionDto[] = [
+    {
+      resource: 'permission',
+      attributes: '*',
+      action: PermissionAction.create_any,
+    },
+    {
+      resource: 'permission',
+      attributes: '*',
+      action: PermissionAction.read_any,
+    },
+    {
+      resource: 'permission',
+      attributes: '*',
+      action: PermissionAction.update_any,
+    },
+    {
+      resource: 'permission',
+      attributes: '*',
+      action: PermissionAction.delete_any,
+    },
+    {
+      resource: 'role',
+      attributes: '*',
+      action: PermissionAction.create_any,
+    },
+    {
+      resource: 'role',
+      attributes: '*',
+      action: PermissionAction.read_any,
+    },
+    {
+      resource: 'role',
+      attributes: '*',
+      action: PermissionAction.update_any,
+    },
+    {
+      resource: 'role',
+      attributes: '*',
+      action: PermissionAction.delete_any,
+    },
+    {
+      resource: 'user',
+      attributes: '*',
+      action: PermissionAction.create_any,
+    },
+    {
+      resource: 'user',
+      attributes: '*',
+      action: PermissionAction.read_any,
+    },
+    {
+      resource: 'user',
+      attributes: '*',
+      action: PermissionAction.update_any,
+    },
+    {
+      resource: 'user',
+      attributes: '*',
+      action: PermissionAction.delete_any,
+    },
+    {
+      resource: 'user',
+      attributes: '*',
+      action: PermissionAction.update_own,
+    },
+    {
+      resource: 'user',
+      attributes: '*',
+      action: PermissionAction.delete_own,
+    },
   ];
-  
+
   await prisma.permission.createMany({ data: permissions });
   await prisma.role.create({
     data: {
       name: 'account',
-      permissions: { connect: [{ id: 14 }, { id: 15 }] },
+      permissions: { connect: [{ id: 13 }, { id: 14 }] },
     },
   });
 }
